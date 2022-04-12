7b05f8ab8e0eef98fa9e42d1949f0aadb76209b2
Andrej <andrej.rakic@elfak.rs>
2022-04-12 20:42:51 +0200

    Update 00_deploy_your_contract.js

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -6,6 +6,7 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
   await deploy("YourContract", {
     from: deployer,
+    args: [process.env.SUBSCRIPTION_ID],
     log: true,
     waitConfirmations: 5
   });
